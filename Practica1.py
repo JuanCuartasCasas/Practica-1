793dcb571c930519f66847849cdcb3c8e7d5b3a0
JuanCuartasCasas <jucuartasc@unal.edu.co>
2025-09-02 19:24:10 -0500

    Practica 1 V2

--- Practica1.py
+++ Practica1.py
@@ -1,1 +1,8 @@
+#Autor:Juan Diego Cuartas Casas
+#Razòn:Inicializacion en github y retroalimentacion conceptos en clase 
 print("Hola mundo,esto es una prueba")
+x="Esto es una prueba de asignacion de variables",2
+print(f"Prueba: {x}",x[1]+2)
+print(type(x))
+str(x)
+print(type(x))
